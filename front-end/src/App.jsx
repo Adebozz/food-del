37bb347ed6f4c681f243b2715fefc21539b72e27
Adebozz <ademolaadeigbe3@gmail.com>
2024-06-04 12:26:39 +0100

    first commit

---
+++ front-end/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbarr from './components/navbar/Navbarr'
+
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbarr />
+    </div>
+  )
+}
+
+export default App
