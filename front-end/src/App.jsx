e9f545c5d156417c04b0f4e4adeab194f8939f36
Adebozz <ademolaadeigbe3@gmail.com>
2024-06-05 18:57:20 +0100

    buiding the header component

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import Navbarr from './components/navbar/Navbarr'
+import { Route, Routes } from 'react-router-dom'
+import Cart from './pages/Cart/Cart'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 
 
 const App = () => {
   return (
     <div className='app'>
       <Navbarr />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/order' element={<PlaceOrder/>} />
+      </Routes>
     </div>
   )
 }
--- front-end/src/components/navbar/Navbarr.jsx
+++ front-end/src/components/navbar/Navbarr.jsx
@@ -9,19 +9,18 @@ const Navbarr = () => {
     <div className='navbar'>
       <img src={assets.logo} alt="" className='logo'/>
       <ul className="navbar-menu">
-        <li className= {menu==="home"?"active":""}>home</li>
-        <li className={menu==="menu"?"active":""}>menu</li>
-        <li className={menu==="mobile-app"?"active":""}>mobile app</li>
-        <li className={menu==="contact-us"?"active":""}>contact us</li>
+        <li onClick={()=>setMenu("home")} className= {menu==="home"?"active":""}>home</li>
+        <li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</li>
+        <li onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile app</li>
+        <li onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</li>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
           <img src={assets.basket_icon} alt="" />
-           
+           <div className="dot"></div>
         </div>
-        <div className="dot"><button>sign in</button></div>
-         
+         <button>sign in</button>
       </div>
     </div>
   )
