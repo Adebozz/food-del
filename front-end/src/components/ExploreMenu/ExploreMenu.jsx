2d8d7315a4c341fb1a853bd96439cf6972ec8835
Adebozz <ademolaadeigbe3@gmail.com>
2024-06-07 20:00:41 +0100

    update on div menu-list

--- front-end/src/components/ExploreMenu/ExploreMenu.jsx
+++ front-end/src/components/ExploreMenu/ExploreMenu.jsx
@@ -7,7 +7,7 @@ const ExploreMenu = () => {
     <div className='explore-menu' id='explore-menu'> 
       <h1>Explore our menu</h1>
       <p className='explore-menu-text'>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest with ingredients and culinary expertise. Our mission is to satify your cravings and elevate your dining experience, pone delicious meal at a time. </p>
-      <di className="menu-list">
+      <div className="menu-list">
         {menu_list.map(()=>{
             return (
                 <div className="explore-menu-list-item"></div>
