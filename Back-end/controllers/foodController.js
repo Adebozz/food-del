5d0a192e554f707a5709e29670f5ae5c4034b29a
Adebozz <ademolaadeigbe3@gmail.com>
2024-06-22 13:41:35 +0100

    working on the upload image function

--- Back-end/controllers/foodController.js
+++ Back-end/controllers/foodController.js
@@ -4,7 +4,22 @@ import fs from 'fs'
 //add food item
 
 const addFood = async (req,res)  => {
+    let image_filename = `${req.file.filename}`;
 
+    const food = new foodModel({
+        name:req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        cartegory: req.body.cartegory,
+        image: image_filename
+    })
+    try {
+        await food.save();
+        res.json({sucess:true,message: "Food Added"})
+    } catch (error) {
+        console.log(error)
+        res.json({success:FileSystemWritableFileStream,message:"Error"})
+    }
 }
 
 export {addFood}
