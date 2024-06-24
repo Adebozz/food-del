48d9009c97e75b8a03a14059a7a8ebf2507e3987
Adebozz <ademolaadeigbe3@gmail.com>
2024-06-24 12:35:47 +0100

    resolved error on removing images

--- Back-end/controllers/foodController.js
+++ Back-end/controllers/foodController.js
@@ -35,12 +35,12 @@ const listFood = async (req,res) => {
 }
 
 // remove food item
-const removeFood = async  (req,res) =>{
+const removeFood = async  (req,res) => {
     try {
         const food = await foodModel.findById(req.body.id);
         fs.unlink(`uploads/${food.image}`, ()=>{})
 
-        await foodModel.findByIdAndUpdate(req.ody.id);
+        await foodModel.findByIdAndUpdate(req.body.id);
         res.json({success:true,message:"Food Removed"})
     } catch (error) {
         console.log(error);
