fe20a0108f082619482c65490bf298e329e9c2c3
Adebozz <ademolaadeigbe3@gmail.com>
2024-06-30 23:22:29 +0100

    update

--- admin/vite-project/src/assets/assets.js
+++ admin/vite-project/src/assets/assets.js
@@ -1,8 +1,8 @@
 import logo from './logo.png'
 import add_icon from './add_icon_green.png'
-import order_icon from './order_icon.png'
-import profile_image from '/profile_image.png'
-import upload_area from './upload_area.png'
+import order_icon from './checkout.png'
+import profile_image from './profile_icon.png'
+import upload_area from './upload.png'
 import parcel_icon from './parcel_icon.png'
 
 export const assets = {
