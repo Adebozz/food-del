3dfe871399f46309fcd3eba16192fec43a8939dd
Adebozz <ademolaadeigbe3@gmail.com>
2024-07-03 23:39:43 +0100

    update on route

--- admin/vite-project/src/App.jsx
+++ admin/vite-project/src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/Sidebar/Sidebar'
+import { Route, Routes } from 'react-router-dom'
+import Order from './pages/Order/order'
+import List from './pages/List/List'
+import Add from './pages/Add/Add'
 
 const App = () => {
   return (
@@ -9,6 +13,11 @@ const App = () => {
       <hr />
       <div className='app-content'>
         <Sidebar />
+        <Routes >
+          <Route path='/add' element={<Add />}/>
+          <Route path='/add' element={<List />}/>
+          <Route path='/add' element={<Order />}/> 
+        </Routes>
       </div>
     </div>
   )
