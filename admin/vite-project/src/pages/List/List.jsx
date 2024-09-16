24651eeab7bd62b6fe7940e009e5a69354fc5a70
Adebozz <ademolaadeigbe3@gmail.com>
2024-09-16 21:12:02 +0100

    fixed the error on the list page

--- admin/vite-project/src/pages/List/List.jsx
+++ admin/vite-project/src/pages/List/List.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './List.css'
 import axios from "axios"
 import {toast} from "react-toastify"
@@ -6,10 +6,11 @@ import {toast} from "react-toastify"
 const List = () => {
 
   const url = "http://localhost:4000"
-  const [list,setList] = usestate([]);
+  const [list,setList] = useState([]);
 
   const fetchList = async () => {
     const response = await axios.get(`${url}/api/food/list`)
+    console.log(response.data)
     if(response.data.success){
       setList(response.data.data);
     }
@@ -18,6 +19,10 @@ const List = () => {
       toast.error("Error")
     }
   }
+
+  useEffect(()=>{
+    fetchList();
+  })
 
   return (
     <div>
